3c30bbcb07ab37335eb1622dec8d90b39b5e2f88
nickjang <nickjang@alumni.upenn.edu>
2020-10-21 13:48:50 -0400

    Setup testing

---
+++ test/app.test.js
@@ -0,0 +1,12 @@
+const supertest = require('supertest');
+const app = require('../app');
+const { expect } = require('chai');
+
+describe('GET /books', () => {
+  it('should return an array of books', () => {
+    return supertest(app)
+      .get('/books')
+      .expect(200)
+      .expect('Content-Type', /json/);
+  });
+});
